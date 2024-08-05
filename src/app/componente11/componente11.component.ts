28fc361670a3c2e9fe68b79cc9991f89070df3fc
m-dantas <mauricio.dantascp@gmail.com>
2024-08-05 17:25:43 -0300

    feat: inclui formulario do projeto

---
+++ src/app/componente11/componente11.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-componente11',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './componente11.component.html',
+  styleUrl: './componente11.component.css'
+})
+export class Componente11Component {
+
+  
+  formulario = new FormGroup({ // Criação do formulario
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]), // Criação da field do formulario
+    idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]), // Validators é um compilado de validadores para o campo em questão (min, max, minLength)
+    cidade: new FormControl('', [Validators.required, Validators.minLength(3)]) 
+  })
+}
